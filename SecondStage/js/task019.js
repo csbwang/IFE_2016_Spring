cc26d2d7c598175bf6628f15260732eaac046c2d
5277 <bwaiqqs@163.com>
2016-04-24 22:30:15 +0800

    Create task019.js

---
+++ SecondStage/js/task019.js
@@ -0,0 +1,90 @@
+function queueInsert(location){
+	var oQueue = document.getElementById('queue');
+	var num = (document.getElementById('input-box').value).match(/^\s*\d+\s*$/);
+	if(num == null || num < 10 || num > 100){
+		alert('输入不合法，请重新输入');
+		return;
+	}
+	if(oQueue.childNodes.length > 60){
+		alert('队列已满，不能再插入');
+		return;
+	}
+	if(location === 'left'){
+		oQueue.innerHTML = '<div style="height:' + num * 5 + 'px"></div>' + oQueue.innerHTML;
+	}
+	else{
+		oQueue.innerHTML = oQueue.innerHTML + '<div style="height:' + num * 5 + 'px"></div>';
+	}
+}
+
+function queueDelete(location){
+	var oQueue = document.getElementById('queue');
+	if(oQueue.childNodes.length < 1){
+		alert('队列为空，无法删除');
+		return;
+	}
+	if(location === 'left'){
+		alert('删除' + (oQueue.firstChild.offsetHeight / 5));
+		oQueue.removeChild(oQueue.firstChild);
+	}
+	else{
+		alert('删除' + (oQueue.lastChild.offsetHeight / 5));
+		oQueue.removeChild(oQueue.lastChild);
+	}
+}
+
+function sort(){
+	var oQueue = document.getElementById('queue');
+	var i, j, len = oQueue.childNodes.length, tmp, isExchanged = false, timer;
+	for(i = 0; i < len - 1; i++){
+		isExchanged = false;
+		for(j = 0; j < len - i - 1; j++){
+			if(oQueue.childNodes[j].offsetHeight > oQueue.childNodes[j + 1].offsetHeight){
+				tmp = oQueue.childNodes[j + 1].offsetHeight;
+				oQueue.childNodes[j + 1].style['height'] = oQueue.childNodes[j].offsetHeight + 'px';
+				oQueue.childNodes[j].style['height'] = tmp + 'px';
+				isExchanged = true;
+			}
+		}
+		if(!isExchanged){
+			return;
+		}
+	}
+}
+
+function init(){
+	var oBtns = document.getElementsByName('button');
+	for(var i = 0; i < oBtns.length; i++){
+		switch(i){
+			case 0:
+				oBtns[i].onclick = function(){
+					queueInsert('left');
+				}
+				continue;
+			case 1:
+				oBtns[i].onclick = function(){
+					queueInsert('right');
+				}
+				continue;
+			case 2:
+				oBtns[i].onclick = function(){
+					queueDelete('left');
+				}
+				continue;
+			case 3:
+				oBtns[i].onclick = function(){
+					queueDelete('right');
+				}
+				continue;
+			case 4:
+				oBtns[i].onclick = function(){
+					sort();
+				}
+				continue;
+			default:
+				alert('有按钮没有绑定事件');
+		}
+	}
+}
+
+window.onload = init;
