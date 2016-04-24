d54c7f5c25277a91aa4ed76f0b56db9de05762f8
5277 <bwaiqqs@163.com>
2016-04-24 16:29:11 +0800

    Update task017.js

--- SecondStage/js/task017.js
+++ SecondStage/js/task017.js
@@ -47,7 +47,7 @@ var chartData = {};
 
 // 记录当前页面的表单选项
 var pageState = {
-  nowSelectCity: -1,
+  nowSelectCity: '北京',
   nowGraTime: "day"
 }
 
@@ -56,10 +56,24 @@ var pageState = {
  */
 function renderChart() {
   var oChartDiv = document.getElementById('aqi-chart-wrap');
+  var graTime = pageState['nowGraTime'], width;
+  
+  switch(graTime){
+    case 'day':
+      width = 5;
+      break;
+    case 'week':
+      width = 10;
+      break;
+    case 'month':
+      width = 15;
+      break;
+  }
   var charStr = '';
-  for(var i = 0; i < chartData.length; i ++){
-    charStr += '<div style="background:black, width:10px, \
-    height:' + chartData[i][1] + 'px" title="' + chartData[i][0] + '"></div>'; 
+  for(var key in chartData){
+    var color = '#'+(Math.random()*0xffffff<<0).toString(16);
+    charStr += '<div style="display:inline-block; background:'+ color +'; width:' + width + 'px; \
+    height:' + chartData[key] + 'px" title="' + key + ': ' + chartData[key] + '"></div>';
   }
   oChartDiv.innerHTML = charStr;
 }
@@ -68,39 +82,46 @@ function renderChart() {
  * 日、周、月的radio事件点击时的处理函数
  */
 function graTimeChange() {
-  // 确定是否选项发生了变化 
-  var oTime = document.getElementById('form-gra-time');
-  var oInputs = oTime.getElementsByTagName('input');
-  var isChanged = false;
-  for(var i = 0; i < oInputs.length; i ++){
-    if(oInputs[i].style['checked'] === 'checked' && oInputs[i].value !== pageState['nowGraTime']){
-      isChanged = true;
+  var oInputs = document.getElementsByName('gra-time');
+  for(var i = 0; i < oInputs.length; i++){
+    // 确定是否选项发生了变化 
+    if(oInputs[i].checked === true && oInputs[i].value !== pageState['nowGraTime']){
       pageState['nowGraTime'] = oInputs[i].value;
+      break;
     }
   }
   // 设置对应数据
-  if(isChanged){
-
-  }
+  initAqiChartData();
   // 调用图表渲染函数
+  renderChart();
 }
 
 /**
  * select发生变化时的处理函数
  */
 function citySelectChange() {
-  // 确定是否选项发生了变化 
-
+  var oSelect = document.getElementById('city-select').value;
+    // 确定是否选项发生了变化 
+  if(oSelect !== pageState['nowSelectCity']){
+    pageState['nowSelectCity'] = oSelect;
+  }
   // 设置对应数据
-
+  initAqiChartData();
   // 调用图表渲染函数
+  renderChart();
 }
 
 /**
  * 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
  */
 function initGraTimeForm() {
-
+  var oField = document.getElementById('form-gra-time');
+  oField.onclick = function(e){
+    var oEvent = e || window.event;
+    if(oEvent.target.nodeName === 'INPUT'){
+      graTimeChange();
+    }
+  }
 }
 
 /**
@@ -108,9 +129,14 @@ function initGraTimeForm() {
  */
 function initCitySelector() {
   // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
-
+  var oSelect = document.getElementById('city-select');
+  var cityStr = '';
+  for(var key in aqiSourceData){
+    cityStr += '<option>' + key + '</option>';
+  }
+  oSelect.innerHTML = cityStr;
   // 给select设置事件，当选项发生变化时调用函数citySelectChange
-
+  oSelect.onchange = citySelectChange;
 }
 
 /**
@@ -119,6 +145,61 @@ function initCitySelector() {
 function initAqiChartData() {
   // 将原始的源数据处理成图表需要的数据格式
   // 处理好的数据存到 chartData 中
+  chartData = {};
+  var tmpData = aqiSourceData[pageState['nowSelectCity']];
+  var key, preKey, curWeekDay, numOfDays = 0, aqiNum = 0, dateStr = '', link;
+  var preMonth = -1, curMonth;
+  if(pageState['nowGraTime'] === 'day'){
+      chartData = tmpData;
+    }
+    else{
+      if(pageState['nowGraTime'] === 'week'){
+        for(key in tmpData){
+          curWeekDay = new Date(key).getDay();
+          if(curWeekDay === 1 && numOfDays !== 0){
+            link = dateStr === '' ? '' : '~';
+            dateStr += link + preKey;
+            chartData[dateStr] = aqiNum / numOfDays;
+            numOfDays = 0;
+            dateStr = key;
+            aqiNum = 0;
+          }
+          else{
+            numOfDays +=1;
+            aqiNum += tmpData[key];
+            preKey = key;
+          }
+        }
+        if(numOfDays !== 0){
+          link = dateStr === '' ? '' : '~';
+          dateStr += link + preKey;
+          chartData[dateStr] = aqiNum / numOfDays;
+        }
+      }
+      else{
+            for(key in tmpData){
+              curMonth = new Date(key).getMonth();
+              if(curMonth !== preMonth && numOfDays !== 0){
+                link = dateStr === '' ? '' : '~';
+                dateStr += link + preKey;
+                chartData[dateStr] = aqiNum / numOfDays;
+                numOfDays = 0;
+                dateStr = key;
+                aqiNum = 0;
+              }
+              else{
+                numOfDays +=1;
+                aqiNum += tmpData[key];
+                preMonth = curMonth;
+              }
+            }
+            if(numOfDays !== 0){
+              link = dateStr === '' ? '' : '~';
+              dateStr += link + preKey;
+              chartData[dateStr] = aqiNum / numOfDays;
+            }
+      }
+    }
 }
 
 /**
@@ -128,6 +209,7 @@ function init() {
   initGraTimeForm()
   initCitySelector();
   initAqiChartData();
+  renderChart();
 }
 
-init();
+window.onload = init;
